064f70e7272501d60181c363c89115dd2992e6b9
bennyqp <66516706+bennyqp@users.noreply.github.com>
2025-04-21 11:06:36 +0200

    Initial Commit

---
+++ YeOldBackup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YeOldBackup
+//
+//  Created by Benedikt Falkenstein on 21.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YeOldBackup/YeOldBackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeOldBackupApp.swift
+//  YeOldBackup
+//
+//  Created by Benedikt Falkenstein on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct YeOldBackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
